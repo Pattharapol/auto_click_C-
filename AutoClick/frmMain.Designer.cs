fcd4f5ad9ee368b170ddffc0d13fdebd444049ce
unknown <tjtik83@gmail.com>
2021-10-24 15:25:33 +0700

    first commit

---
+++ AutoClick/frmMain.Designer.cs
@@ -0,0 +1,119 @@
+﻿
+namespace AutoClick
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblCount = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.lblRandom = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblCount
+            // 
+            this.lblCount.AutoSize = true;
+            this.lblCount.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCount.Location = new System.Drawing.Point(61, 18);
+            this.lblCount.Name = "lblCount";
+            this.lblCount.Size = new System.Drawing.Size(43, 17);
+            this.lblCount.TabIndex = 1;
+            this.lblCount.Text = "label2";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(64, 80);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 35);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStop.Location = new System.Drawing.Point(145, 80);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 35);
+            this.btnStop.TabIndex = 3;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // timer
+            // 
+            this.timer.Interval = 500;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            // 
+            // lblRandom
+            // 
+            this.lblRandom.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRandom.Location = new System.Drawing.Point(61, 48);
+            this.lblRandom.Name = "lblRandom";
+            this.lblRandom.Size = new System.Drawing.Size(43, 17);
+            this.lblRandom.TabIndex = 4;
+            this.lblRandom.Text = "label2";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(288, 148);
+            this.Controls.Add(this.lblRandom);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lblCount);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Auto Click POPCAT";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.frmMain_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblCount;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Timer timer;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label lblRandom;
+    }
+}
+
---
+++ AutoClick/frmMain.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutoClick
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        [DllImport("user32")]
+        public static extern int SetCursorPos(int x, int y);
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            //this.TopMost = true;
+            lblCount.Text = "0";
+        }
+
+        private const int MOUSEEVENTF_MOVE = 0x0001; /* mouse move */
+        private const int MOUSEEVENTF_LEFTDOWN = 0x0002; /* left button down */
+        private const int MOUSEEVENTF_LEFTUP = 0x0004; /* left button up */
+        private const int MOUSEEVENTF_RIGHTDOWN = 0x0008; /* right button down */
+
+        [DllImport("user32.dll",
+            CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+
+        private int count = 1;
+        private int totalCount = 0;
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            var t = Task.Run(() =>
+            {
+                while (count != 0)
+                {
+                    Random random = new Random();
+                    int width = Screen.PrimaryScreen.Bounds.Width;
+                    int height = Screen.PrimaryScreen.Bounds.Height;
+
+                    int cursorHeight = random.Next(150, height - 150);
+                    int cursorWidth = random.Next(350, width - 350);
+
+                    int randInt = random.Next(100000, 500000);
+                    //int randInt = random.Next(800, 1500);
+                    this.BeginInvoke(new ThreadStart(delegate
+                    {
+                        lblRandom.Text = $"{randInt.ToString()}";
+                    }));
+
+                    SetCursorPos(cursorWidth, cursorHeight);
+                    //Thread.Sleep(180000);
+                    Thread.Sleep(randInt);
+                    //Thread.Sleep(5000);
+                    mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 300, 300, 0);
+                    mouse_event(MOUSEEVENTF_LEFTUP, 0, 300, 300, 0);
+                    this.BeginInvoke(new ThreadStart(delegate
+                    {
+                        lblCount.Text = totalCount.ToString() + " clicks";
+                    }));
+
+                    totalCount++;
+                }
+            });
+
+            //SetCursorPos(85, 55);
+            //timer.Start();
+            //while (count != 0)
+            //{
+            //    //if (count == 50)
+            //    //{
+            //    //    count = 1;
+            //    //    SetCursorPos(85, 55);
+            //    //    mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 85, 55, 0);
+            //    //    mouse_event(MOUSEEVENTF_LEFTUP, 0, 85, 55, 0);
+            //    //    Thread.Sleep(3000);
+            //    //}
+            //    //else
+            //    //{
+            //    //    lblCount.Text = totalCount.ToString() + " clicks";
+            //    //    lblCount.Update();
+            //    //    SetCursorPos(200, 200);
+            //    //    Thread.Sleep(500);
+            //    //    mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 300, 300, 0);
+            //    //    mouse_event(MOUSEEVENTF_LEFTUP, 0, 300, 300, 0);
+            //    //}
+            //    //count++;
+            //    //totalCount++;
+
+            //    lblCount.Text = totalCount.ToString() + " clicks";
+            //    lblCount.Update();
+            //    SetCursorPos(800, 450);
+            //    Thread.Sleep(180000);
+            //    mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 300, 300, 0);
+            //    mouse_event(MOUSEEVENTF_LEFTUP, 0, 300, 300, 0);
+            //}
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+        }
+
+        private void frmMain_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                btnStop_Click(sender, e);
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            count = 0;
+            Thread.Sleep(10);
+        }
+    }
+}
